bcbd5ba8b066c86bfb06c3182afae274a8571b24
Janderson Oliveira Pacheco <83798201+jandersonpacheco@users.noreply.github.com>
2021-05-14 01:11:32 -0300

    Teste de revert

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,8 +5,9 @@ public class PrimeiraClasseJava {
 	/*Atalho ctrl + espaço*/
 	/*Main é um método auto executável em Java*/
 	public static void main(String[] args) {
-	/*Escreve texto no console*/	
-	System.out.println("Primeiro projeto em Java");	
+	/*Escreve texto no console*/
+	public int idade
+	System.out.println("Primeiro projeto em Java!");	
 	}
 
 }
